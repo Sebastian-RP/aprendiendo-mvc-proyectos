0f974f87876446d62cc0e329f0b7c170bec17ea3
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-07-19 20:37:17 -0500

    first commit

---
+++ IntroduccionABoostrap/IntroduccionABoostrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroduccionABoostrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Portafolio/Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Persona persona = new Persona()
+            {
+                Nombre = "Gianluigi Buffon",
+                Edad = 41
+            };
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
